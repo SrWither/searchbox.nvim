cf32a4cc7a857efc8c96abcf3119f4572506b440
Fadel <59105868+SrWither@users.noreply.github.com>
2022-02-01 13:34:56 -0300

    Update search-types.lua

--- lua/searchbox/search-types.lua
+++ lua/searchbox/search-types.lua
@@ -423,14 +423,16 @@ M.confirm = function(value, state)
       cursor_pos({pos.end_line, pos.end_col - 15})
     end
 
-    menu.confirm_action({
-      on_close = function()
-        clear_matches(state)
-      end,
-      on_submit = function(item)
-        fn.execute(item, pos)
-      end
-    })
+    vim.defer_fn(function()
+      menu.confirm_action({
+        on_close = function()
+          clear_matches(state)
+        end,
+        on_submit = function(item)
+          fn.execute(item, pos)
+        end
+      })
+    end, 5)
   end
 
   fn.confirm(next_match())
